75bb11420b83bcbfa7078f5c3a65e8b3cf133ab3
TrinityWeaver <sebastian.marynicz@gmail.com>
2022-07-27 22:44:04 +0100

    init

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">=0.12.0"
+  backend "s3" {
+    region  = "eu-west-2"
+    profile = "private-sebastian"
+    key     = "tf-sebastian-resume-project"
+    bucket  = "tf-sebastian-resume-project"
+
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "region-master" {
+  type    = string
+  default = "eu-west-2"
+}
+
+variable "region-virginia" {
+  type    = string
+  default = "us-east-1"
+}
+
+
+variable "profile" {
+  type    = string
+  default = "private-sebastian"
+}
+
+
