5779906fa4f900f53a0ee553c56b300a09a061bd
TrinityWeaver <sebastian.marynicz@gmail.com>
2022-09-11 19:55:37 +0100

    remove outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "s3_website_domain" {
-  value       = aws_s3_bucket_website_configuration.resume_project_static.website_endpoint
-  description = "S3 website domain"
-}
 
 
 output "resume_url" {
