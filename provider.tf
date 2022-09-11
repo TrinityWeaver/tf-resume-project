c6f27243ad989629876af3464d89c5baeb368d0f
TrinityWeaver <sebastian.marynicz@gmail.com>
2022-09-11 18:32:13 +0100

    comment out "profile"

--- provider.tf
+++ provider.tf
@@ -1,19 +1,19 @@
 provider "aws" {
-  profile = var.profile
+  #profile = var.profile
   region  = var.region-master
   alias   = "region-master"
 }
 
 
 provider "aws" {
-  profile = var.profile
+  #profile = var.profile
   region  = var.region-virginia
   alias   = "region-virginia"
 }
 
 
 provider "aws" {
-  profile = var.profile
+  #profile = var.profile
   region  = var.region-ire
   alias   = "region-ire"
 }
